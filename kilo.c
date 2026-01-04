d66cab1972e811b70983ad689c3dc5c91f46c054
Sky Tran <skytraan@gmail.com>
2026-01-03 21:04:37 -0800

    Implemented an incremental search function & fixed semantic issues

--- kilo.c
+++ kilo.c
@@ -66,7 +66,7 @@ struct editorConfig E;
 /*** prototypes ***/
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 /*** terminal ***/
 void die(const char *s) {
@@ -198,6 +198,19 @@ int editorRowCxToRx(erow *row, int cx) {
     return rx;
 }
 
+int editorRowRxToCx(erow *row, int rx) {
+    int cur_rx = 0;
+    int cx;
+    for (cx = 0; cx < row->size; cx++){
+        if (row->chars[cx] = '\t')
+            cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+        cur_rx++;
+
+        if (cur_rx > rx) return cx;
+    }
+    return cx;
+}
+
 void editorUpdateRow(erow *row){
     int tabs = 0;
     int j;
@@ -283,7 +296,7 @@ void editorRowDelChar(erow *row, int at){
 /*** editor operations ***/
 void editorInsertChar(int c) {
     if (E.cy == E.numrows){
-        editorAppendRow(E.numrows, "", 0);
+        editorInsertRow(E.numrows, "", 0);
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
@@ -297,7 +310,7 @@ void editorInsertNewLine(){
         editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
         row = &E.row[E.cy];
         row->size = E.cx;
-        row->chars[row->size] = "\0";
+        row->chars[row->size] = '\0';
         editorUpdateRow(row);
     }
     E.cy++;
@@ -314,7 +327,7 @@ void editorDelChar(){
         E.cx--;
     } else {
         E.cx = E.row[E.cy - 1].size;
-        editorROwAppendString(&E.row[E.cy-1], row->chars, row->size);
+        editorRowAppendString(&E.row[E.cy-1], row->chars, row->size);
         editorDelRow(E.cy);
         E.cy--;
     }
@@ -355,7 +368,7 @@ void editorOpen(char *filename){
         while (linelen > 0 && (line[linelen-1] == '\n' ||
                                 line[linelen-1] == '\r'))
             linelen--;
-        editorAppendRow(E.numrows, line, linelen);
+        editorInsertRow(E.numrows, line, linelen);
     }
     free(line);
     fclose(fp);
@@ -364,7 +377,7 @@ void editorOpen(char *filename){
 
 void editorSave(){
     if (E.filename == NULL){
-        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
         if (E.filename == NULL){
             editorSetStatusMessage("Save aborted");
             return;
@@ -389,6 +402,64 @@ void editorSave(){
 
     free(buf);
     editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+
+/*** find ***/
+
+void editorFindCallback(char *query, int key){
+    static int last_match = -1;
+    static int direction = 1;
+
+    if (key == '\r' || key == '\x1b'){
+        last_match = -1;
+        direction = 1;
+        return;
+    } else if (key == ARROW_RIGHT || key == ARROW_DOWN){
+        direction = 1;
+    } else if (key == ARROW_LEFT || key == ARROW_UP){
+        direction = -1;
+    } else {
+        last_match = -1;
+        direction = 1;
+    }
+
+    if (last_match == -1) direction = 1;
+    int current = last_match;
+    int i;
+    for (i = 0; i<E.numrows; i++){
+        current += direction;
+        if (current == -1) current = E.numrows - 1;
+        else if (current == E.numrows) current = 0;
+
+        erow *row = &E.row[current];
+        char *match = strstr(row->render, query);
+        if (match) {
+            last_match = current;
+            E.cy = current;
+            E.cx = editorRowRxToCx(row, match - row->render);
+            E.rowoff = E.numrows;
+            break;
+        }
+    }
+}
+
+void editorFind() {
+    int saved_cx = E.cx;
+    int saved_cy = E.cy;
+    int saved_coloff = E.coloff;
+    int saved_rowoff = E.rowoff;
+
+    char *query = editorPrompt("Search: %s (ESC/Arrows/Enter)", 
+                                editorFindCallback);
+    
+    if(query) {
+        free(query);
+    } else {
+        E.cx = saved_cx;
+        E.cy = saved_cy;
+        E.coloff = saved_coloff;
+        E.rowoff = saved_rowoff;
+    }
 }
 
 /*** append buffer ***/
@@ -532,7 +603,7 @@ void editorSetStatusMessage(const char *fmt, ...){
 
 /*** input ***/
 
-char *editorPrompt(char *prompt) {
+char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
     size_t bufsize = 128;
     char *buf = malloc(bufsize);
 
@@ -548,11 +619,13 @@ char *editorPrompt(char *prompt) {
             if (buflen != 0) buf[--buflen] = '\0';
         } else if (c == '\x1b'){
             editorSetStatusMessage("");
+            if (callback) callback(buf,c);
             free(buf);
             return NULL;
         } else if (c == '\r'){
             if (buflen != 0){
                 editorSetStatusMessage("");
+                if (callback) callback(buf, c);
                 return buf;
             }
         } else if (!iscntrl(c) && c < 128){
@@ -563,6 +636,7 @@ char *editorPrompt(char *prompt) {
             buf[buflen++] = c;
             buf[buflen] = '\0';
         }
+        if (callback) callback(buf,c);
     }
 }
 void editorMoveCursor(int key){
@@ -639,6 +713,10 @@ void editorProcessKeypress(){
             }
             break;
         
+        case CTRL_KEY('f'):
+            editorFind();
+            break;
+        
         case BACKSPACE:
         case CTRL_KEY('h'):
         case DEL_KEY:
@@ -706,7 +784,8 @@ int main(int argc, char *argv[]){
         editorOpen(argv[1]);
     }
 
-    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
+    editorSetStatusMessage(
+        "HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
     while (1){
         editorRefreshScreen();
